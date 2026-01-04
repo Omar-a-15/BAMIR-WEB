2c2904c9860b285bb2f5bea9afbb83810abcc41d
Omar Batista <obatista035@gmail.com>
2026-01-04 14:58:00 -0500

    hamburguesa y scroll suave listos

--- Pagina WEB - BAMIR/assets/js/script.js
+++ Pagina WEB - BAMIR/assets/js/script.js
@@ -5,3 +5,19 @@ toggle.addEventListener("click", () => {
   nav.classList.toggle("active");
   toggle.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav a").forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault(); // evita el salto brusco
+    const targetId = link.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // cerrar menú en móvil
+    nav.classList.remove("active");
+    toggle.classList.remove("active");
+  });
+});
